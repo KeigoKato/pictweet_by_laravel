ac8b60e5c8e21a3564f7a0024f19b82f53ae1fdf
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-16 14:43:17 +0900

    [add] 全アクションのリンクをヘッダーに追加
    開発中はこれが便利

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,10 @@
 @section("header")
 <header>
     <h1>@yield("title")</h1>
+    <a href="index">indexアクション</a>
+    <a href="find">findアクション</a>
+    <a href="add">addアクション</a>
+    <a href="edit">editアクション</a>
+    <a href="del">delアクション</a>
 </header>
 @endsection
